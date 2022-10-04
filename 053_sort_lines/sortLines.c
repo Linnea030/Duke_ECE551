a7e9b82c1d889bd9c763d888f49e9ceee0b53053
yc538 <yc538@duke.edu>
2022-10-03 21:18:25 -0400

    Done assignment 53

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,44 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-int main(int argc, char ** argv) {
-  //WRITE YOUR CODE HERE!
+void sortfile(FILE * f) {
+  size_t i = 0;
+  size_t sz = 0;
+  char ** lines = NULL;
+  char * curr = NULL;
+  while (getline(&curr, &sz, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
 
+int main(int argc, char ** argv) {
+  if (argc == 1) {
+    FILE * f = stdin;
+    sortfile(f);
+  }
+  if (argc > 1) {
+    for (size_t k = 1; k < argc; k++) {
+      FILE * f = fopen(argv[k], "r");
+      if (f == NULL) {
+        perror("No such file\n");
+        exit(EXIT_FAILURE);
+      }
+      sortfile(f);
+      if (fclose(f) != 0) {
+        perror("The input file can not be closed\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
