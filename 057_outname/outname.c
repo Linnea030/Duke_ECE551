a09e6437ff0e2c8e04567e150aef81c1d74c0161
yc538 <yc538@duke.edu>
2022-10-11 19:30:06 -0400

    Done assignment 57

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -7,10 +7,10 @@
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
   size_t size = strlen(inputName);
-  //char * add = ".count\0";
+  char * add = ".counts\0";
   char * output;  // = malloc((size + strlen(add)) * sizeof(*output));
   output = strndup(inputName, size);
   output = realloc(output, (size + 8) * sizeof(*output));
-  strcpy(output + size, ".count\0");
+  strcpy(output + size, add);
   return output;
 }
