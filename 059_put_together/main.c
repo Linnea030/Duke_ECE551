ccb58cd45f40d0720851214b9d94f59b281f3f37
yc538 <yc538@duke.edu>
2022-10-13 19:30:59 -0400

    Done assignment 58

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -2,74 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "assert.h"
 #include "counts.h"
 #include "kv.h"
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
   FILE * f = fopen(filename, "r");
   if (f == NULL) {
-    fprintf(stderr, "No such file");
+    fprintf(stderr, "could not open the file\n");
     return NULL;
   }
-  counts_t * word = createCounts();
-  char * line = NULL;
-  size_t sz = 0;
-  while (getline(&line, &sz, f) >= 0) {
-    //lookupValue(kvPairs, line);
-    char * line1 = strchr(line, '\n');
-    if (line1 != NULL) {
-      *line1 = '\0';
+  counts_t * answer = createCounts();
+  //for every key in file
+  char * key = NULL;
+  size_t sz;
+  while (getline(&key, &sz, f) >= 0) {
+    char * p = strchr(key, '\n');
+    if (p != NULL) {
+      *p = '\0';
     }
-    addCount(word, lookupValue(kvPairs, line));
-    // free(line);
-    // line = NULL;
+    char * value = lookupValue(kvPairs, key);
+    addCount(answer, value);
   }
-  free(line);
-  //int n = fclose(f);
-  if (fclose(f) != 0) {
-    fprintf(stderr, "Failed to close");
-    exit(EXIT_FAILURE);
-  }
-  return word;
+  free(key);
+  int result = fclose(f);
+  assert(result == 0);
+  return answer;
 }
 
 int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
-  //read the key/value pairs from the file named by argv[1] (call the result kv)
   if (argc < 2) {
-    fprintf(stderr, "no argc");
-    exit(EXIT_FAILURE);
+    fprintf(stderr, "lack of arguments\n");
+    return EXIT_FAILURE;
   }
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
   kvarray_t * kv = readKVs(argv[1]);
   //count from 2 to argc (call the number you count i)
   for (int i = 2; i < argc; i++) {
-    counts_t * c = countFile(argv[i], kv);
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
-    //   (call this result c)
-
+    counts_t * c = countFile(argv[i], kv);
     //compute the output file name from argv[i] (call this outName)
     char * outName = computeOutputFileName(argv[i]);
     //open the file named by outName (call that f)
     FILE * f = fopen(outName, "w");
     if (f == NULL) {
-      fprintf(stderr, "No such file");
-      exit(EXIT_FAILURE);
+      fprintf(stderr, "could not open the newfile\n");
+      abort();
     }
-    //print the counts from c into the FILE f
-    printCounts(c, f);
-    //close f
-    //int n = fclose(f);
-    if (fclose(f) != 0) {
-      fprintf(stderr, "Failed to close");
-      exit(EXIT_FAILURE);
-    }
+    printCounts(c, f);  //print the counts from c into the FILE f
+    int result = fclose(f);
+    assert(result == 0);
     //free the memory for outName and c
     free(outName);
     freeCounts(c);
   }
-  //free the memory for kv
-  freeKVs(kv);
+  freeKVs(kv);  //free the memory for kv
   return EXIT_SUCCESS;
 }
