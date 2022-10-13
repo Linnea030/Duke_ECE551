789c7e41072003f9aad0ffccfbb2b51568195f45
yc538 <yc538@duke.edu>
2022-10-13 17:38:37 -0400

    Done assignment 59

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -19,6 +19,7 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   while (getline(&line, &sz, f) > 0) {
     char * name = lookupValue(kvPairs, line);
     addCount(word, name);
+    line = NULL;
   }
   free(line);
   int n = fclose(f);
