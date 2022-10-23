5a1ffeadedac4c096dd1c387e4d27bdd53214a9c
yc538 <yc538@duke.edu>
2022-10-23 01:56:55 -0400

    Done assignment 60

---
+++ 060_eval2/rand_story.h
@@ -0,0 +1,22 @@
+#ifndef __RAND_STORY_H__
+#define __RAND_STORY_H__
+
+#include "provided.h"
+
+//any functions you want your main to use
+
+/*FILE * f = fopen(argv[1], "r");
+char * line = NULL;
+char ** larray = NULL;
+size_t sz = 0;
+size_t i = 0;
+while (getline(&line, &sz, f) >= 0) {
+  larray = realloc(larray, (i + 1) * sizeof(larray));
+  larray[i] = line;
+  line = NULL;
+  i++;
+}
+free(line);
+}*/
+
+#endif
---
+++ 060_eval2/story-step1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rand_story.h"
+
+int main(int argc, char ** argv) {
+  //If there are not two command line arguments
+  if (argc != 2) {
+    fprintf(stderr, "argc invalid\n");
+    exit(EXIT_FAILURE);
+  }
+  //If there is no input file
+  if (argv[1] == NULL) {
+    fprintf(stderr, "No input file\n");
+    exit(EXIT_FAILURE);
+  }
+  //using function to parsing the story
+
+  FILE * f = fopen(argv[1], "r");
+  //open file is fail
+
+  char * line = NULL;
+  char ** larray = NULL;
+  size_t sz = 0;
+  size_t i = 0;
+  while (getline(&line, &sz, f) >= 0) {
+    larray = realloc(larray, (i + 1) * sizeof(larray));
+    larray[i] = line;
+    line = NULL;
+    i++;
+  }
+  free(line);
+
+  for (size_t j = 0; j < i; j++) {
+    char * res;
+    size_t len = strlen(larray[j]);
+    size_t len_res = 0;
+    const char * cword;
+    // int num_under=0;
+    for (size_t k = 0; k < len; k++) {
+      if (larray[j][k] != '_') {
+        //	num_under++;
+        res = realloc(res, (len_res + 1) * sizeof(*res));
+        res[len_res] = larray[j][k];
+        len_res++;
+      }
+      else {
+        cword = chooseWord(NULL, NULL);
+        size_t len_c = strlen(cword);
+        k++;
+        for (size_t m = 0; m < len_c; m++) {
+          res = realloc(res, (len_res + 1) * sizeof(*res));
+          res[len_res] = cword[m];
+          len_res++;
+        }
+        while (larray[j][k] != '_') {
+          k++;
+          if (k >= len) {
+            fprintf(stderr, "No matching underscore");
+            exit(EXIT_FAILURE);
+          }
+        }
+        //	num_under++;
+      }
+    }
+    res = realloc(res, (len_res + 1) * sizeof(res));
+    res[len_res] = '\0';
+    printf("%s", res);
+    free(res);
+  }
+  for (size_t j = 0; j < i; j++) {
+    free(larray[j]);
+  }
+  free(larray);
+  return EXIT_SUCCESS;
+}
