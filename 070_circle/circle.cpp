0fed31d3513b5fa24c1760d2c934c712c63d700d
yc538 <yc538@duke.edu>
2022-10-27 12:09:55 -0400

    Done assignment 70

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -6,26 +6,20 @@ void Circle::move(double dx, double dy) {
 }
 
 double Circle::intersectionArea(const Circle & otherCircle) {
-  double sum = radius + otherCircle.radius;
-  double Rs;
-  double Rl;
-  if (radius < otherCircle.radius) {
-    Rl = otherCircle.radius;
-    Rs = radius;
-  }
-  else {
-    Rl = otherCircle.radius;
-    Rs = radius;
-  }
+  double r1 = radius;
+  double r2 = otherCircle.radius;
   double d = center.distanceFrom(otherCircle.center);
-  double sub = Rl - Rs;
-  if (d >= sum)
-    return 0.0;
-  else if (d <= sub)
-    return M_PI * pow(Rs, 2);
-  else {
-    double ang1 = acos((Rl * Rl + d * d - Rs * Rs) / (2 * Rl * d));
-    double ang2 = acos((Rs * Rs + d * d - Rl * Rl) / (2 * Rs * d));
-    return ang1 * Rl * Rl + ang2 * Rs * Rs - Rl * d * sin(ang1);
+
+  if (d >= r1 + r2)
+    return 0;
+  if (r1 > r2) {
+    double tmp = r1;
+    r1 = r2;
+    r2 = tmp;
   }
+  if (r2 - r1 >= d)
+    return M_PI * r1 * r1;
+  double ang1 = acos((r1 * r1 + d * d - r2 * r2) / (2 * r1 * d));
+  double ang2 = acos((r2 * r2 + d * d - r1 * r1) / (2 * r2 * d));
+  return ang1 * r1 * r1 + ang2 * r2 * r2 - r1 * d * sin(ang1);
 }
