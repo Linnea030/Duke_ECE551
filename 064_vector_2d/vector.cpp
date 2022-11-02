66e6422bcc273d8691afc2007d7b684dbe72b59b
yc538 <yc538@duke.edu>
2022-11-02 16:34:42 -0400

    test

--- 064_vector_2d/vector.cpp
+++ 064_vector_2d/vector.cpp
@@ -9,22 +9,27 @@ void Vector2D::initVector(double init_x, double init_y) {
   x = init_x;
   y = init_y;
 }
+
 double Vector2D::getMagnitude() const {
-  return std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+  return sqrt(pow(x, 2) + pow(y, 2));
 }
+
 Vector2D Vector2D::operator+(const Vector2D & rhs) const {
   Vector2D res;
-  res.initVector(rhs.x + this->x, rhs.y + this->y);
+  res.initVector(rhs.x + x, rhs.y + y);
   return res;
 }
+
 Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
   x = x + rhs.x;
   y = y + rhs.y;
   return *this;
 }
+
 double Vector2D::dot(const Vector2D & rhs) const {
   return x * rhs.x + y * rhs.y;
 }
+
 void Vector2D::print() const {
   printf("<%.2f, %.2f>", x, y);
 }
